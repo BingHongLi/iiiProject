a760f9eba913c14273b14407d3dd515f860bf55d
BingHongLi <b97607065@gmail.com>
2014-09-14 21:11:56 +0800

    contentAndXML1

--- Crawler/CrawlerLBHaboutXML1.py
+++ Crawler/CrawlerLBHaboutXML1.py
@@ -1,48 +1,59 @@
-'''
-以先前的CrawlerLBH1 抓取steam臺灣區所有連結
-而後進行抓取金額XML的動作，而後按照種類及編號存成檔案
-並計算成功數量與失敗數量
-尚有精進的空間，如video歸類成sub或app的部分。
-'''
-#設定計數器
+#初版
+#設定套件
+import requests
+from bs4 import BeautifulSoup
+import HTMLParser
+import urlparse
+import time
+import re
+from math import ceil
+'''test='http://store.steampowered.com/sub/17001/?snr=1_7_7_230_150_1'
+category=re.search('(\w+):(...)(\w+).(\w+).(\w+).(\w+).(\w+)',test)
+print category.group(6),category.group(7)
+dataFormat='http://steamsales.rhekua.com/xml/sales/%s_%d.xml?curr=78'
+print dataFormat%(category.group(6),int(category.group(7)))'''
 success=0
 fail=0
-#讀取連結檔案
-f=open('test3.txt','r')
-#針對每一行連結進行如下動作
+f=open('test2.txt')
 for i in f.readlines():
-    #正規表示法取得編號
     category=re.search('(\w+):(...)(\w+).(\w+).(\w+).(\w+).(\w+)',i)
-    #組成儲存路徑及檔案名
-    tempFile='try/'+category.group(6)+'_'+category.group(7)+'.txt'
+    #tempFile='try/'+category.group(6)+'_'+category.group(7)+'.txt'
     #print tempFile
     rs=requests.session()
-    #進行辨認，如果取得字串為video，將轉成app，進行讀取，但video仍有可能為sub或其他，故仍有404的發生可能
     if(category.group(6)=='video'):
         dataFormat='http://steamsales.rhekua.com/xml/sales/app_%d.xml?curr=78'
-        rs=requests.session()
         response=rs.get(dataFormat%(int(category.group(7))))
-        #若沒發生404，則進行建檔的動作
         if(response.status_code!=404):
+            tempFile='try/'+'app_'+category.group(7)+'.txt'
+            content='content/'+'app_'+category.group(7)+'.txt'
             t=open(tempFile,'w')
             t.write(response.text)
+            t.close()
+            t=open(content,'w')
+            temp2=rs.get(i).text.encode('utf8')
+            t.write(temp2)
             t.close()
             success=success+1
         else:
             fail=fail+1
-    #取得字串非video，進行如下動作。
     else:
         dataFormat='http://steamsales.rhekua.com/xml/sales/%s_%d.xml?curr=78'
         response=rs.get(dataFormat%(category.group(6),int(category.group(7))))
         if(response.status_code!=404):
+            tempFile='try/'+category.group(6)+'_'+category.group(7)+'.txt'
+            content='content/'+category.group(6)+'_'+category.group(7)+'.txt'
             t=open(tempFile,'w')
             t.write(response.text)
+            t.close()
+            t=open(content,'w')
+            temp2=rs.get(i).text.encode('utf8')
+            t.write(temp2)
             t.close()
             success=success+1
         else:
             fail=fail+1    
     print tempFile
+    print content
     print response.status_code
     print success,fail
-#關閉檔案串流
 f.close()
