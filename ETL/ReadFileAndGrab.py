1776de11158bf025c786963a3aba9f88f373dc38
BingHongLi <b97607065@gmail.com>
2014-09-17 18:10:26 +0800

    Version 2

--- ETL/ReadFileAndGrab.py
+++ ETL/ReadFileAndGrab.py
@@ -11,13 +11,19 @@ for i in t:
 content=BeautifulSoup(x)
 #遊戲標題名
 #print content.find('div',{'class':'apphub_AppName'}).text
+#contentHeader=content.find('div',{'class':'apphub_AppName'}).text
+
 #使用者認為的特性
+
 '''chara=content.find('div',{'class':'glance_tags popular_tags'}).findAll('a')
 for i in chara:
-    print i.text'''
+    #print i.text
+    #print i.text.replace('\n','').replace('\t','')
+    charaText=i.text.replace('\n','').replace('\t','')'''
 
-#遊戲描述
-#print content.find('div',{'id':'game_area_description'}).text
+#遊戲描述 建議使用第一個
+#print content.find('div',{'id':'game_area_description'}).text.replace('\t','').replace('\n','') 
+#print content.find('div',{'id':'game_area_description'}).text#.replace('\t','').replace('\n','')
 
 #找出遊戲描述
 '''for i in content.findAll('div',{'id':'game_area_description'}):
@@ -27,9 +33,20 @@ for i in chara:
 '''for i in content.findAll('div',{'id':'game_area_sys_req'}):
     print i.text'''
 
+'''for i in range(len(content.findAll('div',{'id':'game_area_sys_req_full'}))):
+    print content.findAll('div',{'id':'game_area_sys_req'})[i].find('h2').text.replace('\t','').replace('\n','').replace(' ','')
+    print content.findAll('div',{'id':'game_area_sys_req_full'})[i].text.replace('\t','').replace('\n','')'''
+#print content.find('div',{'id':'game_area_sys_req'}).find('h2').text.replace('\t','').replace('\n','')
+#print content.find('div',{'id':'game_area_sys_req_full'}).text
+
 #找出開發商的資訊、發售日、發行語言
 #print content.find('div',{'class':'details_block'}).text
+'''print content.find('div',{'class':'details_block'}).text.replace('\t','').replace('\n','')
+text = content.find('table',{'class':'game_language_options'}).findAll('tr')[1:]
+for a in text:
+    print a.text'''
 
 #找出評論，未完成，網頁間id可能不同，不能套用
 '''if content.find('div',{'id':'review_box partial'}) is None:
     print content.find('div',{'id':'Reviews_all'}).text'''
+
