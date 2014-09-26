5edb3fe6e1a038bc2df8cb933745ce7ede3c3625
dsakryk <dsakryk@gmail.com>
2014-09-26 21:31:50 +0800

    fix a bug

--- Crawler/isthereanydeal_to_MetaCritic.py
+++ Crawler/isthereanydeal_to_MetaCritic.py
@@ -11,8 +11,8 @@ def game_info_ajaxs(input_name, output_folder):
     game_info = soup.find_all('div', {'class' : 'link'})[1]
 
     if game_info.text == 'You can try your luck searching by yourself, though.':
-        with open(output_folder + 'mabey_null.txt', 'a') as file_mabey_null:
-            file_mabey_null.write(link + ' |||| ' + soup.find('div', {'id':'pageContent'}).h2.text + '\n')
+        with open(output_folder + 'mabey_null.txt', 'a') as file_maybe_null:
+            file_maybe_null.write(link + ' |||| ' + soup.find('div', {'id':'pageContent'}).h2.text + '\n')
     elif re.compile (r'MetaCritic.com'):
         with open(output_folder + 'MetaCritic_link.txt', 'a') as file_MetaCritic_link:
             file_MetaCritic_link.write(game_info.a['href'] + ' |||| ' + soup.find('div', {'id':'pageContent'}).h2.text  + '\n')
