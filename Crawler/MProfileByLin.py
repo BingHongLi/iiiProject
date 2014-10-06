d32dc38c525f5028507335929478e83402762a36
ThomasLin <o51753007@gmail.com>
2014-10-07 00:03:36 +0800

    ReadOK

--- Crawler/MProfileByLin.py
+++ Crawler/MProfileByLin.py
@@ -72,77 +72,108 @@ if __name__=='__main__':
 #coding
 from bs4 import BeautifulSoup
 import time
+import re
 
 def MetacriticCrawler(inputUrl):         #定義輸入method，輸出HTML
     if __name__=='__main__':        
         
-        mGameName = inputUrl.split('/')[5]                                                                         #找出遊戲名稱
+        mGameName = inputUrl.split('/')[5]                                                                                                    #找出遊戲名稱
         print mGameName
         
-        splider=BrowserBase()                                                                                      #呼叫偽裝機器人                
-        soup = BeautifulSoup(splider.openurl(inputUrl+'/details').read())                                          #抓下HTML
+        splider=BrowserBase()                                                                                                              #呼叫偽裝機器人                
+        soup = BeautifulSoup(splider.openurl(inputUrl+'/details').read())                                           #抓下HTML
          
-        print ('----------------------------------------------------------------------------------introduce--------------------------------------------------------------------------------------')
-        introduce=soup.findAll('div',{"class":"summary_detail product_summary"})                                   #introduce  
         
-        for i in introduce:
-            print ' '.join(i.text.split())
+        print ('----------------------------------------------------------------------------------introduce--------------------------------------------------------------------------------------')
+        #print soup.findAll('div',{"class":"summary_detail product_summary"})    
+        if soup.findAll('div',{"class":"summary_detail product_summary"}):
+            introduce=soup.findAll('div',{"class":"summary_detail product_summary"})                         #introduce  
+            for i in introduce:
+                print ' '.join(i.text.split())
+        else:
+            introduce=soup.findAll('li',{"class":"summary_detail product_summary"})                         #introduce  
+            for i in introduce:
+                print ' '.join(i.text.split())
         
         print ('---------------------------------------------------------------------------------releaseDate------------------------------------------------------------------------------------')
         
-        print ''.join(soup.findAll('span',{"class":"data"})[1].text.split())                                        #releaseDate
+        print ' '.join(soup.findAll('span',{"class":"data"})[1].text.split())                                        #releaseDate
         print ('----------------------------------------------------------------------------------metascore&userscore-------------------------------------------------------------------')    
         
-        metascore = soup.find('div',{"class":"metascore_wrap feature_metascore"}).text.split()[1]                   #metascore
+        metascore = soup.find('div',{"class":"metascore_wrap feature_metascore"}).text.split()[1]               #metascore
         print (metascore)
         
         
-        userscore = soup.find('div',{"class":"userscore_wrap feature_userscore"}).text.split()[2]                   #userscore
+        userscore = soup.find('div',{"class":"userscore_wrap feature_userscore"}).text.split()[2]                               #userscore
         print (userscore)
         
         print ('---------------------------------------------------------------------------developer & gameTag-----------------------------------------------------------------------------')    
-        developer = soup.find('table',{"cellspacing":"0"})                                                          #developer & gameTag
+        developer = soup.find('table',{"cellspacing":"0"})                                                                                       #developer & gameTag
                
-        for tag in developer.findAll('tr'):                                                                         #逐一取出表格內容
+        for tag in developer.findAll('tr'):                                                                                                      #逐一取出表格內容
             information = ''.join(tag.text.split())
             print ''.join(information.split(':')[0])
             print ''.join(information.split(':')[1])
         
         print ('==================================================/critic-reviews-===========================================-')    
-        time.sleep(1)
-        soup2 = BeautifulSoup(splider.openurl(inputUrl+'/critic-reviews').read())                                   #gameReview
+        #time.sleep()
+        soup2 = BeautifulSoup(splider.openurl(inputUrl+'/critic-reviews').read())                                         #gameReview
         content = soup2.find('div',{"class":"body product_reviews"})
         
         i=0
-        j = content.find('div',{"class":"msg msg_no_reviews"})
-                 
-        while j is None:                                                                                            #逐一取出內容
-            #if content.findAll('div',{"class":"date"})[i] is True:
-            print content.findAll('div',{"class":"date"})[i].text
-            #if content.findAll('div',{'class':'source'})[i] is True:
-            print content.findAll('div',{'class':'source'})[i].text
-            #if content.findAll('div',{'class':'review_body'})[i] is True:
-            print ' '.join(content.findAll('div',{'class':'review_body'})[i].text.split())
-            i+=1
-            if content.findAll('div',{"class":"review_body"}).__len__() == i:
-                break
+        # help from 品中
+        def match_class(target):                                                        
+            def do_match(tag):                                                          
+                classes = tag.get('class', [])                                          
+                return all(c in classes for c in target)                                
+            return do_match  
+
+        if content.find('div',{"class":"msg msg_no_reviews"}):
+            print None
+        else:    
+            while content.find('div',{"class":"msg msg_no_reviews"}) is None:                                                                                                            #逐一取出內容
+                if content.findAll('div',{"class":"date"}):
+                    print content.findAll('div',{"class":"date"})[i].text
+                if content.findAll('div',{'class':'source'}):
+                    print content.findAll('div',{'class':'source'})[i].text
+                if content.findAll('div',{'class':'review_body'}):
+                    print ' '.join(content.findAll('div',{'class':'review_body'})[i].text.split())
+                
+               
+                if content.findAll(match_class(["metascore_w", "medium", "game"])):
+                    print content.findAll(match_class(["metascore_w", "medium", "game"]))[i].text
+                
+                
+                #if content.findAll('div',{'class':re.compile("metascore_w medium game")}):
+#                    print content.findAll('div',{'class':re.compile("metascore_w medium game")})[i].text
+                '''
+                if content.findAll('div',{'class':'metascore_w medium game mixed indiv}):
+                    print content.findAll('div',{'class':'metascore_w medium game mixed indiv'})[].text
+                if content.findAll('div',{'class':'metascore_w medium game negative indiv'}):
+                    print content.findAll('div',{'class':'metascore_w medium game negative indiv'}).text
+                '''  
+                    
+                i+=1
+                if content.findAll('div',{"class":"review_body"}).__len__() == i:
+                    break
+        
+          
                 
 #MetacriticCrawler method End
                 
-'''    
-a= 'http://www.metacritic.com/game/pc/the-sims-3?full_summary=1'
-MetacriticCrawler(a) 
-'''
-
+    
+#a= 'http://www.metacritic.com/game/pc/the-sims-3'
+#MetacriticCrawler(a) 
+                
+    
 
 f =open('MetaCritic_link_.txt','r')                         #open MetaCritic_link_.txt
 
 for i in range(0,f.readlines().__len__()):
-   link =index[i].split('|||||')[0]
+   link =index[i].split('|||||')[0].split('?full_summary=1')[0]
    MetacriticCrawler(link)
     
 f.close
-
 
 
 
