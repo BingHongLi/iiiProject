7b448c764653f225525a17645a3f2d1c95af4a14
BingHongLi <b97607065@gmail.com>
2014-10-09 13:43:49 +0800

    ExtractSubToCSV

--- ETL/Steam/ExtractSubContent.py
+++ ETL/Steam/ExtractSubContent.py
@@ -3,6 +3,7 @@ import HTMLParser
 import json
 import os
 import re
+import unicodedata
 
 for dirPath, dirNames, AllfileNames in os.walk("C://Users/BigData/Desktop/sub/"):
     for fileName in AllfileNames:
@@ -10,9 +11,9 @@ for dirPath, dirNames, AllfileNames in os.walk("C://Users/BigData/Desktop/sub/")
         #print os.path.join(dirPath, f)
         a=re.search('(\w+)',fileName)
         ID=a.group(1)
-        gameProfile={ID:{'ID':ID,'gameName':'','introduce':'','releaseDate':'',
-                 'score':'','developer':'','publisher':'',
-                 'systemRequire':''}}
+        gameProfile={ID:{"ID":ID,"gameName":"null","introduce":"null","releaseDate":"null",
+                 "score":"NA","developer":"null","publisher":"null",
+                 "systemRequire":"null"}}
         #讀檔案至暫存區，等待解析
         targetFilePath="C://Users/BigData/Desktop/sub/"+fileName
         of=open(targetFilePath,'r')
@@ -56,7 +57,7 @@ for dirPath, dirNames, AllfileNames in os.walk("C://Users/BigData/Desktop/sub/")
                         #print i.text
                         tempDevelop+=i.text
                         #print tempDevelop 
-                        gameProfile[ID]['developer']=tempDevelop
+                        gameProfile[ID]['developer']=tempDevelop.encode('utf8')
     
                 #發行商
                 elif htmlevent.b.text == 'Publisher:':
@@ -65,18 +66,24 @@ for dirPath, dirNames, AllfileNames in os.walk("C://Users/BigData/Desktop/sub/")
                     for i in temp:
                         #print i.text  
                         tempPublish+=i.text
-                        gameProfile[ID]['publisher']=tempDevelop
+                        gameProfile[ID]['publisher']=tempDevelop.encode('utf8')
                 #發行日
                 elif htmlevent.b.text == 'Release Date:':
                     temp=str(htmlevent)
-                    gameProfile[ID]['releaseDate']=temp.split('<b>Release Date:</b>')[1].replace('\t','')
+                    gameProfile[ID]['releaseDate']=temp.split('<b>Release Date:</b>')[1].replace('\t','').encode('utf8')
       
 
-        gameProfileJson=json.dumps(gameProfile[ID], indent = 4)
-        #print gameProfileJson
-        targetFilePath='E://iiiProject/data/subContent/'+ID+'.json'
-        print targetFilePath
-        testFile=open(targetFilePath,'w')
-        testFile.write(gameProfileJson)
+        #gameProfileJson=json.dumps(gameProfile[ID], indent = 4)
+        #final.append(gameProfileJson)
+        addContent='"'+gameProfile[ID]['ID']+'","'+gameProfile[ID]['gameName']+'","'+gameProfile[ID]['score']+'","'+gameProfile[ID]['releaseDate']+'","'+gameProfile[ID]['developer']+'","'+gameProfile[ID]['publisher']+'","'+gameProfile[ID]['systemRequire']+'","'+gameProfile[ID]['introduce']+'"'
+        #print gameProfile[ID]['score']
+        #print gameProfile[ID]
+        addContent=addContent.decode('utf8')
+        addContent=unicodedata.normalize('NFKD', addContent).encode('ascii','ignore')
+        targetFilePath='E://iiiProject/data/newExtract/subContent.csv'
+        #testFile=open(targetFilePath,'w')
+        testFile=open(targetFilePath,'a')
+        testFile.write(addContent+"\r\n")
+        #testFile.write('['+','.join(final)+']')
         testFile.close()
         
