44a3aa160fe23c87171ada71c5f4b30e46e8ee62
BingHongLi <b97607065@gmail.com>
2014-10-21 01:29:48 +0800

    clusterAndClassificate

--- WebDesign/timeSeriesShiny/server.R
+++ WebDesign/timeSeriesShiny/server.R
@@ -1,28 +1,53 @@
 library(shiny)
+library(rpart.plot)
 ### Loading slow
 ALLPRICE <- read.csv('./data/SPrice.csv')
 ALLPROFILE <- read.csv('./data/SProfile.csv')
+CLUSTERPRICE = readRDS('./data/allPrice.RDS')
+CLUSTER_1496 = read.csv('./data/CLUSTER_1496.csv')
+ALLCLUSTER <- read.csv('./data/ClusterClassifier.csv')
 
 ### find Developer's game
-
 FINDDEVELOPERPROFILE <- function(developerName,queryFile=ALLPROFILE){
   developerFile <- queryFile[queryFile[,5]==developerName,]
   developerFile
 }
 
-##fine game price history
+### fine game price history
 FINDDEVELOPERGAMEPRICELIST <- function(gameName,queryFile,queryPrice=ALLPRICE){
   gameID <- as.character(queryFile[which(queryFile[,2]==gameName),1])
   priceHistory <- queryPrice[which(queryPrice[,1]==gameID),3]
   priceHistory
 }
 
-DRAWCURVE <- function(priceHisory){
-  priceTimeSeries <- ts(priceHisory)
+### Draw Curve
+DRAWCURVE <- function(priceHistory){
+  # TimeSerise Data
+  priceTimeSeries <- ts(priceHistory)
+  # Draw curve
   plot.ts(priceTimeSeries,col="red")
+  
 }
 
+### Cluster Compare return a rpart's list
+CLUSTERCOMPARE <- function(cluster1,cluster2){
+  
+  C1<- ALLCLUSTER[which(ALLCLUSTER[,3]==cluster1),]
+  C2<- ALLCLUSTER[which(ALLCLUSTER[,3]==cluster2),]
+  ALLCLUSTERATTR <- rbind(C1,C2)
+  myFomula <- CLARA~Accounting+Adventure+Casual+Education+Indie+Racing+RPG
+  test_rpart <- rpart(myFomula,method="class",data=ALLCLUSTERATTR,control=rpart.control(minsplit=10))
+  #plot(test_rpart)
+  #text(test_rpart,use.n = TRUE, all=T)
+  #rpart.plot(test_rpart)
+}
+
+
+
 shinyServer(function(input, output, session) {
+  
+  #############For control  ##########
+  
   output$control1 <- renderUI({
     selectInput("developerName", "Select Developer Name", choices = levels(ALLPROFILE[['developer']]))
   })
@@ -37,11 +62,52 @@ shinyServer(function(input, output, session) {
     selectInput("gameName", "Select Game Name", choices = choice2)
   })
   
-  output$plotTS <- renderPlot({
-     developerFile<- FINDDEVELOPERPROFILE(input$developerName)
-     priceHistory<- FINDDEVELOPERGAMEPRICELIST(input$gameName,developerFile)
-     drawCurve<- DRAWCURVE(priceHistory)
-     drawCurve
+  output$control3 <- renderUI({
+    selectInput("clusterName", "Select cluster Name", choices = as.character(c(1:20)))
   })
+  
+  output$control4 <- renderUI({
+    xx <- input$clusterName
+    if (any(
+      is.null(xx)
+    ))
+      return("Select")
+    selectInput("clusterGameName", "Select Game Name", choices = as.character(CLUSTER_1496['X'][,1][which(CLUSTER_1496['clara']== as.numeric(xx))]))
+  })
+  
+  output$control5 <- renderUI({
+    selectInput("clusterNumber1", "Select Cluster Number", choices =c(1:20),selected=1 )
+  })
+  
+  output$control6 <- renderUI({
+    selectInput("clusterNumber2", "Select other Cluster Number ", choices = c(1:20),selected=2)
+  })
+  
+  #############For plot  ############
+  
+  ### find Developer's game and draw curve
+  output$plotTS <- renderPlot({
+    # generate develper's profile
+    developerFile<- FINDDEVELOPERPROFILE(input$developerName)
+    
+    # generate gamePrice's list
+    priceHistory<- FINDDEVELOPERGAMEPRICELIST(input$gameName,developerFile)
+    
+    # draw
+    drawCurve<- DRAWCURVE(priceHistory)
+    drawCurve
+  })
+  
+  ### depend on your choice cluster and draw curve
+  output$plotClusterTS <- renderPlot({
+    drawCurve <- DRAWCURVE(CLUSTERPRICE[[input$clusterGameName]])
+    drawCurve
+  })
+  
+  ### draw decision tree
+  output$plotCluster <- renderPlot({
+    rpart.plot(CLUSTERCOMPARE(input$clusterNumber1,input$clusterNumber2))
+  })
+  
   
 })
--- WebDesign/timeSeriesShiny/ui.R
+++ WebDesign/timeSeriesShiny/ui.R
@@ -1,24 +1,66 @@
 library(shiny)
-shinyUI(fluidPage(
-  title="GameAnalysis",
-  titlePanel("GameTrendAnalysis"),
-  fluidRow(
-    column(4,
-           tabsetPanel(
-             title="PriceTrend",
-             type="pills",
-             tabPanel(
-               uiOutput("control1")
-             ),
-             tabPanel(
-               uiOutput("control2")
-             )        
-           )
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("PlotTS",plotOutput("plotTS"))
-      )
-    ) 
-  )
-))
+library(rpart.plot)
+shinyUI(navbarPage("",
+                   tabPanel("Price Trend",
+                            sidebarLayout(
+                                sidebarPanel(
+                                    tabsetPanel("GameAnalysis",
+                                                title="PriceTrend",
+                                                type="pills",
+                                                tabPanel(
+                                                    uiOutput("control1")
+                                                ),
+                                                tabPanel(
+                                                    uiOutput("control2")
+                                                )                
+                                    )
+                                ),
+                                mainPanel(
+                                    plotOutput("plotTS")
+                                )
+                            )
+                   ),tabPanel("Price Cluster",
+                              sidebarLayout(
+                                  sidebarPanel(
+                                      tabsetPanel("GameAnalysis",
+                                          title="PriceCluster",
+                                          type="pills",
+                                          tabPanel(
+                                              uiOutput("control3")
+                                          )
+                                          ,
+                                          tabPanel(
+                                              uiOutput("control4")
+                                          )
+                                      )
+                                  ),
+                                  mainPanel(
+                                      plotOutput("plotClusterTS")
+                                  )
+                              )
+                   ), tabPanel("ClusterAnalysis",
+                            sidebarLayout(
+                                    sidebarPanel(
+                                            tabsetPanel("ClusterAnalysis",
+                                                  title="ClusterAnalysis",
+                                                         type="pills",
+                                                         tabPanel(
+                                                            uiOutput("control5")
+                                                         ),
+                                                         tabPanel(
+                                                            uiOutput("control6")
+                                                         )    
+                                                     )
+                                                   ),
+                                            mainPanel(
+                                                   plotOutput("plotCluster")
+                                                
+                                            )
+                             )
+                                                 
+                   ),
+                   inverse = T, # ??????
+                   theme=NULL # 設???css
+                   #theme='test.css' 
+)
+)
